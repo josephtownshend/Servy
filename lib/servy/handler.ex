569daa1aa338cc1ef89079b3d77b65383212e537
joseph townshend <joetownshend@mac.com>
2019-09-11 17:32:43 +0100

    feat: pattern matching on the routes

--- lib/servy/handler.ex
+++ lib/servy/handler.ex
@@ -7,7 +7,7 @@ defmodule Servy.Handler do
     |> format_response
   end
 
-  def log(conv), do:  IO.inspect conv
+  def log(conv), do: IO.inspect conv
 
   def parse(request) do
     [method, path, _] =
@@ -20,11 +20,18 @@ defmodule Servy.Handler do
   end
 
   def route(conv) do
+    route(conv, conv.method, conv.path)
+  end
+
+  def route(conv, "GET", "/wildthings") do
     %{ conv | resp_body: "Bears, Lions, Tigers" }
   end
 
+  def route(conv, "GET", "/bears") do
+    %{ conv | resp_body: "Teddy, Smokey, Paddington" }
+  end
+
   def format_response(conv) do
-    # TODO: Use values in the map to create an HTTP response string:
     """
     HTTP/1.1 200 OK
     Content-Type: text/html
@@ -38,6 +45,30 @@ end
 
 request = """
 GET /wildthings HTTP/1.1
+Host: example.com
+User-Agent: ExampleBrowser/1.0
+Accept: */*
+
+"""
+
+response = Servy.Handler.handle(request)
+
+IO.puts response
+
+request = """
+GET /bears HTTP/1.1
+Host: example.com
+User-Agent: ExampleBrowser/1.0
+Accept: */*
+
+"""
+
+response = Servy.Handler.handle(request)
+
+IO.puts response
+
+request = """
+GET /bigfoot HTTP/1.1
 Host: example.com
 User-Agent: ExampleBrowser/1.0
 Accept: */*
