0ed281d9273a73eb96b6902702e059504ed72000
joseph townshend <joetownshend@mac.com>
2019-09-12 09:27:25 +0100

    moved 404 to end

--- lib/servy/handler.ex
+++ lib/servy/handler.ex
@@ -23,16 +23,16 @@ defmodule Servy.Handler do
     route(conv, conv.method, conv.path)
   end
 
-  def route(conv, _method, path) do
-    %{ conv | resp_body: "No #{path} here"}
-  end
-
   def route(conv, "GET", "/wildthings") do
     %{ conv | resp_body: "Bears, Lions, Tigers" }
   end
 
   def route(conv, "GET", "/bears") do
     %{ conv | resp_body: "Teddy, Smokey, Paddington" }
+  end
+
+  def route(conv, _method, path) do
+    %{ conv | resp_body: "No #{path} here"}
   end
 
   def format_response(conv) do
